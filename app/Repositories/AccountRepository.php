9bfd7ecf69634683b5eba64a02c1ce742b91395b
Taleh <t4lex999@gmail.com>
2023-08-24 23:09:36 +0400

    Account repository

--- app/Repositories/AccountRepository.php
+++ app/Repositories/AccountRepository.php
@@ -5,11 +5,30 @@ namespace App\Repositories;
 
 use App\Models\Account;
 use App\Repositories\Contracts\IAccountRepository;
+use App\ValueObjects\Money;
+use Illuminate\Support\Collection;
 
 class AccountRepository extends BaseRepository implements IAccountRepository
 {
-    public function __construct(Account $account)
+    public function __construct(Account $model)
     {
-        parent::__construct($account);
+        parent::__construct($model);
+    }
+
+    public function getAllByClientId(string $userId): Collection
+    {
+        return $this->model->where('user_id', $userId)->get();
+    }
+
+    public function getById(string $accountId): ?Account
+    {
+        return $this->model->find($accountId);
+    }
+
+    public function updateBalance(Account $account, Money $amount): void
+    {
+        // Assuming you have a 'balance' attribute in your Account model
+        $account->balance = $amount;
+        $account->save();
     }
 }
--- app/Repositories/Contracts/IAccountRepository.php
+++ app/Repositories/Contracts/IAccountRepository.php
@@ -3,7 +3,15 @@ declare(strict_types=1);
 
 namespace App\Repositories\Contracts;
 
+use App\Models\Account;
+use App\ValueObjects\Money;
+use Illuminate\Support\Collection;
+
 interface IAccountRepository
 {
+    public function getAllByClientId(string $clientId): Collection;
+
+    public function getById(string $accountId): ?Account;
+    public function updateBalance(Account $account, Money $amount): bool;
 
 }
