b3b20b230e650f2e60637ce179c9c4abc829c483
Taleh <t4lex999@gmail.com>
2023-08-25 19:27:57 +0400

    Transaction model refacotor

--- app/Models/Transactions.php
+++ app/Models/Transactions.php
@@ -2,14 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Transaction extends Model
+class Transactions extends Model
 {
     use HasFactory;
 
+    use HasUuids;
+    protected $fillable = [
+        'sender_account_id', // Add other fillable fields if needed
+        'receiver_account_id',
+        'amount',
+        'currency',
+        'exchange_rate'
+
+    ];
     public function senderAccount(): BelongsTo
     {
         return $this->belongsTo(Account::class, 'sender_account_id');
