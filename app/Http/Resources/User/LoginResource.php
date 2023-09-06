a5d58bbd9e3f03193a91df1ab029333358d64aa3
Taleh <t4lex999@gmail.com>
2023-09-07 01:56:22 +0400

    User Based

---
+++ app/Http/Resources/User/LoginResource.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Resources\User;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LoginResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'message' => 'User logged in successfully',
+            'user' => [
+                'id' => $this->id,
+                'name' => $this->name,
+                'email' => $this->email,
+                // Add any additional login-specific data here
+            ],
+            'token' => $this->token, // Include the authentication token if needed
+        ];
+    }
+}
+
---
+++ app/Http/Resources/User/RegistrationResource.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Resources\User;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RegistrationResource extends  JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'message' => 'User registered successfully',
+            'user' => [
+                'id' => $this->id,
+                'name' => $this->name,
+                'email' => $this->email,
+                // Add any additional registration-specific data here
+            ],
+        ];
+    }
+}
