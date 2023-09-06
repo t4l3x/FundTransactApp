cd38528710f63938b6922ffdb389279f142d7482
Taleh <t4lex999@gmail.com>
2023-09-07 01:57:40 +0400

    Service provider Cache

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\CacheService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(CacheService::class, function ($app) {
+            return new CacheService();
+        });
     }
 
     /**
